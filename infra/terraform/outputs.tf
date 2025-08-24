aac333e794d4e0bd093381fd2d65edb05d68bbb6
Vivek Kumar <vivekltp120@gmail.com>
2025-08-24 17:11:18 +0530

    reformat variables

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,6 +1,55 @@
-output "ecr_url" { 
-    value = aws_ecr_repository.api.repository_url
-     }
-output "alb_dns_name" { 
-    value = module.alb.lb_dns_name 
-    }
+##############################
+# ALB Outputs
+##############################
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = module.alb.dns_name
+}
+
+output "alb_arn" {
+  description = "ARN of the ALB"
+  value       = module.alb.arn
+}
+
+output "alb_arn_suffix" {
+  description = "ARN suffix of the ALB (for CloudWatch metrics)"
+  value       = module.alb.arn_suffix
+}
+
+##############################
+# Target Group Outputs
+##############################
+
+output "target_group_arn" {
+  description = "ARN of the ECS target group"
+  value       = aws_lb_target_group.api.arn
+}
+
+##############################
+# ECS Outputs
+##############################
+
+output "ecs_cluster_id" {
+  description = "ECS Cluster ID"
+  value       = module.ecs.cluster_id
+}
+
+output "ecs_service_name" {
+  description = "ECS Service name"
+  value       = aws_ecs_service.api.name
+}
+
+output "ecs_task_definition" {
+  description = "Task definition ARN for ECS service"
+  value       = aws_ecs_task_definition.api.arn
+}
+
+##############################
+# ECR Outputs
+##############################
+
+output "ecr_repository_url" {
+  description = "ECR repository URL for pushing images"
+  value       = aws_ecr_repository.api.repository_url
+}

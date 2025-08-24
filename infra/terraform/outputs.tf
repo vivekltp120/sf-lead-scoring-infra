b175916c576e3c9d4d7908b424c7cc7fb0199ba2
Vivek Kumar <vivekltp120@gmail.com>
2025-08-24 15:44:59 +0530

    reformat tf variable

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -2,5 +2,5 @@ output "ecr_url" {
     value = aws_ecr_repository.api.repository_url
      }
 output "alb_dns_name" { 
-    value = module.alb.lb_dns_name 
+    value = module.alb.this_lb_dns_name 
     }

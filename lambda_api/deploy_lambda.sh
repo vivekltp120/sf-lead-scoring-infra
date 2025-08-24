4bce219b238bdbde0f290e161b320f8238340191
Vivek Kumar <vivekltp120@gmail.com>
2025-08-24 15:58:38 +0530

    reformat for lambda function file

--- lambda_api/deploy_lambda.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-zip -r lambda_package.zip lambda_function.py
-
-aws lambda create-function --function-name lead-scoring-lambda   --zip-file fileb://lambda_package.zip --role arn:aws:iam::947288527335:role/sagemaker --runtime python3.9 --handler lambda_function.lambda_handler

88c0f64eb4fff51764567ce23837c5bcdd53a7b0
Vivek Kumar <vivekltp120@gmail.com>
2025-08-25 15:41:21 +0530

    restructured for aws

--- src/inference.py
+++ src/inference.py
@@ -13,17 +13,18 @@ def model_fn(model_dir):
     booster.load_model(path)
     return booster
 
-def input_fn(body, content_type):
-    if content_type == "application/json":
-        payload = json.loads(body)
-        # Expect {"instances":[[...],[...]]}
-        arr = np.asarray(payload["instances"], dtype=float)
-        return arr
-    raise ValueError(f"Unsupported content type: {content_type}")
+# def input_fn(body, content_type):
+#     if content_type == "application/json":
+#         payload = json.loads(body)
+#         # Expect {"instances":[[...],[...]]}
+#         arr = np.asarray(payload["instances"], dtype=float)
+#         return arr
+#     raise ValueError(f"Unsupported content type: {content_type}")
 
 def predict_fn(data, model):
     # For XGBClassifier
-    preds = model.predict_proba(data)[:,1]
+    input_data=np.array(data)
+    preds = model.predict_proba(input_data)
     return preds
 
 def output_fn(prediction, accept):

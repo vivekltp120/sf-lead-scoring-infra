9af2c65a99fcf847166fea60ebc29b36f17d8914
Vivek Kumar <vivekltp120@gmail.com>
2025-08-27 15:29:41 +0530

    s3 path for xgboost model modified

--- src/inference.py
+++ src/inference.py
@@ -6,7 +6,7 @@ import xgboost as xgb
 
 
 # A recommended filename for the XGBoost model within the tarball is 'xgboost_model.json'
-MODEL_FILENAME = "xgboost_model.json"
+MODEL_FILENAME = "xgboost-model"
 
 def model_fn(model_dir):
     path = os.path.join(model_dir, MODEL_FILENAME)
